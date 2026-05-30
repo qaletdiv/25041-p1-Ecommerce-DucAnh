b030c121de4467624d64b19a01f18a0ccfd6bcbe
Tu Duc Anh <tuducanhdz0806@gmail.com>
2026-05-30 18:42:38 +0700

    Edit file login.js

--- login/login.js
+++ login/login.js
@@ -23,11 +23,11 @@ function bindEvent() {
     });
 
     document.getElementById('email').addEventListener('input', function () {
-        clearFielError('email');
+        clearFieldError('email');
         clearGeneralError();
     });
     document.getElementById('password').addEventListener('input', function () {
-        clearFielError('password');
+        clearFieldError('password');
         clearGeneralError();
     });
 
@@ -36,6 +36,42 @@ function bindEvent() {
         btn.addEventListener('click', function () {
             const emailEl = document.getElementById('email');
             const passwordEl = document.getElementById('password');
-        })
+            if (emailEl) emailEl.value = this.dataset.enail;
+            if (passwordEl) passwordEl.value = this.dataset.password;
+            clearAllError();
+        });
+    });
+}
+
+
+// Xu ly dang nhap
+function handleLoginSubmit(e) {
+    e.preventDefault();
+    clearAllError();
+
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim(); 
+
+    const errors = validateLoginForm(email,password);
+    if (errors.length > 0) {
+        errors.forEach(function(err) {
+            showFieldError(err.field, err.msg);
+        }); 
+        return;
+    }
+
+    setButtonLoadding(true);
+
+    setTimeout(function () {
+        const user = authenticateUser(email,password);
+
+        if (!user) {
+            setButtonLoadding(false);
+            showGenerationError('Email hoặc mật khẩu không chính xác. Vui lòng thử lại.');
+            document.getElementById('group-password')?.claslist.add('shake');
+            setTimeout(function () {
+                document
+            })
+        }
     });
 }
